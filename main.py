270adcfbdbd79a1ebb813d972cc48d3311ee6a75
Harish Devathraj <42981316+devathrajharish@users.noreply.github.com>
2025-01-28 14:44:00 +0530

    pypi

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pexels_api_python.pexels_api import PexelsAPI
 
 # Initialize the API with your API key
-api_key = "your_api_key"
+api_key = "API_KEY"
 pexels = PexelsAPI(api_key)
 
 # Search for photos
@@ -9,8 +9,8 @@ search_results = pexels.search_photos("nature")
 print(search_results)
 
 # Get details of a specific photo
-photo_details = pexels.get_photo(12345)  # Replace with a valid photo ID
-print(photo_details)
+#photo_details = pexels.get_photo(12345)  # Replace with a valid photo ID
+#print(photo_details)
 
 # Get popular photos
 popular_photos = pexels.get_popular_photos()
